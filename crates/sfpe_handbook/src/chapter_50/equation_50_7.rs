7f1c69b5467efb682f31e82764663ab32a09446b
simonsantama <simonsantama@gmail.com>
2026-01-31 17:59:34 +0000

    Very minor change to Latex in 50.7

--- crates/sfpe_handbook/src/chapter_50/equation_50_7.rs
+++ crates/sfpe_handbook/src/chapter_50/equation_50_7.rs
@@ -6,7 +6,7 @@ pub fn effective_area(a_sr: f64, a_ir: f64, a_io: f64) -> f64 {
 pub fn effective_area_equation(a_sr: String, a_ir: String, a_io: String) -> String {
     format!(
         "{} = \\left(\\dfrac{{1}}{{{}}}^2 + \\dfrac{{1}}{{{}}}^2 + \\dfrac{{1}}{{{}}}^2\\right)^{{-0.5}}",
-        "A_{effective}", a_sr, a_ir, a_io
+        "A_{e}", a_sr, a_ir, a_io
     )
 }
 
